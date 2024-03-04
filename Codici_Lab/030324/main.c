63b62ca27b1e9ab044505090ff7493dc6eda671d
RMChannel <98057423+RMChannel@users.noreply.github.com>
2024-03-04 14:01:36 +0100

    Lab 030324

---
+++ Codici_Lab/030324/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "vettore.h"
+#define MAXELEM 100
+
+int main(int argc, char *argv[]) {
+    int scelta=1;
+    int elements=0;
+    int n[100]={0};
+    int temp;
+    while (1) {
+        printf("Scegli un'opzione:\n[1]Inserisci elementi nell'array\n[2]Stampa tutti gli elementi dell'array\n[3]Ricerca un elemento nell'array\n[4]Trova il minimo nell'array\n[5]Ordina l'array\n[altro]Esci\n");
+        scanf("%d",&scelta);
+        switch(scelta) {
+            case 1:
+                printf("Inserite il numero di elementi da aggiungere: ");
+                scanf("%d",&temp);
+                elements+=temp;
+                input_array(n,temp);
+                break;
+            case 2:
+                output_array(n,elements);
+                break;
+            case 3:
+                printf("Inserite un elemento intero da cercare: ");
+                scanf("%d",&temp);
+                ricerca_array(n,temp,elements);
+                break;
+            case 4:
+                printf("Il minimo è %d\n",n[min_array(n,elements)]);
+                break;
+            case 5:
+                ordinaArray(n,elements);
+                printf("Ordinamento eseguito\n");
+                break;
+            default:
+                return 0;
+        }
+    }
+}
+/*
+Mancano:
+[6]Elimina un elemento dell'array
+[7]Aggiungi un singolo elemento dell'array
+*/
---
+++ Codici_Lab/030324/vettore.h
@@ -0,0 +1,6 @@
+void input_array(int a[], int n);
+void output_array(int a[], int n);
+void ricerca_array(int a[], int n, int max);
+int min_array(int a[], int n);
+void swap(int *a, int *b);
+void ordinaArray(int *a,int n);
