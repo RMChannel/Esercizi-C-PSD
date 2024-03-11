3ebef61e3767e0f8a86d9d9c56bb0e67258762f8
RMChannel <98057423+RMChannel@users.noreply.github.com>
2024-03-11 17:14:15 +0100

    LAB2

---
+++ Codici_Lab/110324/driverEs2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vettore.h"
+#define N 100
+
+int main(int argc, char *argv[]) {
+    //Apertura e controllo dell'esistenza dei file
+    FILE *fp_input, *fp_oracle, *fp_output;
+    if ((fp_input=fopen(argv[1],"r"))==NULL) {
+        printf("File input non trovato\n");
+        exit(EXIT_FAILURE);
+    }
+    if ((fp_oracle=fopen(argv[2],"r"))==NULL) {
+        printf("File oracolo non trovato\n");
+        exit(EXIT_FAILURE);
+    }
+    if ((fp_output=fopen(argv[3],"w"))==NULL) {
+        printf("File output non trovato\n");
+        exit(EXIT_FAILURE);
+    }
+    /*
+    dichiaro le variabili, qui mi servono 3 array, ovvero i 2 di input e quello di controllo dell'oracolo,
+    inoltre mi serve pos
+    */
+    char line[N]="";
+    int n_input1[N]={0};
+    int n_input2[N]={0};
+    int n_oracle[N]={0};
+    int imax1, imax2, pos;
+    for (int i=1;fgets(line,N,fp_input)!=NULL;i++) {
+        pos=0;
+        imax1=input_array_str(n_input1,line,&pos); //mi salvo i due array con le loro dimensioni
+        imax2=input_array_str(n_input2,line,&pos);
+        somma2Array(n_input1,n_input2,&imax1,&imax2); //mi calcolo la somma dei due array e la salvo nel 1°
+        pos=0;
+        fgets(line,N,fp_oracle);
+        input_array_str(n_oracle,line,&pos); //mi salvo l'array di oracle
+        fprintf(fp_output,"Test %d: %d\n",i,controlloOracleArray(n_input1,n_oracle,imax1)); //stampo il controllo nel file di output
+    }
+}
---
+++ Codici_Lab/110324/vettore.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+/*
+input_array_str(...), data una stringa e un array e variabile puntatore pos, essa estrapola da una stringa l'array in esso contenuto.
+Pos serve per spostare la posizione di calcolo dell'array dopo il 1° già scansionato (nel caso sia già stato fatta la scansione del 1°array),
+questo lo controllo tramite l'if dove controllo se if è diverso da 0, se è uguale a 0 allora vuol dire che scansiono il 1°, altrimenti allora sposta
+la linea di pos (tutti i caratteri scansionati prima) più 3 per saltare il carattere di separazione e gli spazi
+*/
+int input_array_str(int *arr, char *line, int *pos) {
+    int i=0, n=0;
+    if (*pos!=0) line+=*pos+3;
+    while ((sscanf(line,"%d%n",&arr[i],&n))==1) {
+        line+=n;
+        *pos+=n;
+        i++;
+    }
+    return i;
+}
+
+/*
+Questa funzione server per ricavare un semplice dato intero, nel nostro caso viene utilizzato per ricavare il valore singolo dato dal file oracolo
+*/
+int getresult(char *line) {
+    int n;
+    sscanf(line,"%d",&n);
+    return n;
+}
+
+/*
+Restituisce la somma di tutti i valori di un array
+*/
+int sommaArray(int *arr, int imax) {
+    int n=0;
+    for (int i=0;i<imax;i++) n+=arr[i];
+    return n;
+}
+
+/*
+Questa funzione si occupa di sommare tutti i valori di due array e salvandoli nel primo,
+l'if serve perché la funzione prevede che i due array possano avere due dimensioni diverse,
+di conseguenza, visto che il for scorre i valori fino ad imax1, solo nel caso che imax1 (dimensione del 1° array)
+sia minore di imax2 (dimensione del 2°array) allora sostituisce imax1 con imax2,
+non c'è pericolo di andare fuori memoria perché gli array sono inizializzati tutti a 0 e di conseguenza anche se un array
+non ha valori per suo limite, sono comunque inizializzati a 0 e di conseguenza facendo la somma di 0, restituisce i valori
+dell'array con i valori inizializzati
+*/
+void somma2Array(int *arr1, int *arr2, int *imax1, int *imax2) {
+    if (*imax1<*imax2) *imax1=*imax2;
+    for (int i=0;(i<*imax1);i++) arr1[i]=arr1[i]+arr2[i];
+}
+
+/*
+Questa funzione controlla che l'array dato e l'array dell'oracolo siano uguali, e che di conseguenza
+il risultato sia corretto o meno
+*/
+int controlloOracleArray(int *arr, int *oracle, int imax) {
+    for (int i=0;i<imax;i++) if (arr[i]!=oracle[i]) return 0;
+    return 1;
+}
+
+/*
+Questa funzione si occupa di fare la sommatoria dei prodotti tra i valori dell'array, ovvero la formula è questa: n=sommatoria(arr1[i]*arr2[i])
+*/
+int sumProd(int *arr1, int *arr2, int imax) {
+    int n=0;
+    for (int i=0;i<imax;i++) n+=(arr1[i]*arr2[i]);
+    return n;
+}
+
+/*
+Stampa l'intero array
+*/
+void outputArray(int *a, int n) {
+	int i;
+	for(i=0; i<n; i++){
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
